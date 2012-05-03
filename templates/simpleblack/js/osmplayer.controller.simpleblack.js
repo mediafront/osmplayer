fa985248d3e338909116f8144e2ea0b9ee0a8629
Travis Tidwell <travist349@gmail.com>
2012-05-02 19:42:59 -0700

    Fixed the simpleblack template to use the latest codebase.

---
+++ templates/simpleblack/js/osmplayer.controller.simpleblack.js
@@ -0,0 +1,102 @@
+(function(template, osmplayer) {
+
+  /** The osmplayer namespace. */
+  var osmplayer = osmplayer || {};
+
+  // Define the controller object.
+  osmplayer.controller = osmplayer.controller || {};
+
+  /**
+   * Constructor for the minplayer.controller
+   */
+  osmplayer.controller[template] = function(context, options) {
+
+    // Derive from default controller
+    minplayer.controller.call(this, context, options);
+  };
+
+  /** Derive from controller. */
+  osmplayer.controller[template].prototype = new minplayer.controller();
+  osmplayer.controller[template].prototype.constructor = osmplayer.controller[template];
+
+  /**
+   * @see minplayer.plugin#construct
+   */
+  osmplayer.controller[template].prototype.construct = function() {
+    minplayer.controller.prototype.construct.call(this);
+    if (!this.options.controllerOnly) {
+      this.get('player', function(player) {
+        this.get('media', function(media) {
+          if (!media.hasController()) {
+            minplayer.showThenHide(this.display, 5000, function(shown) {
+              var op = shown ? 'addClass' : 'removeClass';
+              player.display[op]('with-controller');
+            });
+          }
+          else {
+            player.display.addClass('with-controller');
+          }
+        });
+      });
+    }
+  }
+
+  /**
+   * Return the display for this plugin.
+   */
+  osmplayer.controller[template].prototype.getDisplay = function() {
+
+    // See if we need to build out the controller.
+    if (this.options.build) {
+
+      // Prepend the control template.
+      jQuery('#mediaplayer_minplayer', this.context).append('\
+      <div id="mediaplayer_control">\
+        <div id="mediaplayer_controlLeft">\
+          <div id="mediaplayer_playPause" class="mediaplayer_controlspace">\
+            <div id="mediaplayer_play" class="on"><span>play</span></div>\
+            <div id="mediaplayer_pause" class="off"><span>pause</span></div>\
+          </div>\
+          <div id="mediafront_playtime" class="mediaplayer_controlspace">00:00</div>\
+        </div>\
+        <div id="mediaplayer_controlRight">\
+          <a id="mediafront_resizeScreen" class="mediaplayerlink mediaplayer_controlspace" href="#fullscreen">\
+            <div id="mediafront_resize_to_fullScreen" class="on"><span>make full screen</span></div>\
+            <div id="mediafront_resize_to_normalScreen" class="off"><span>make normal screen</span></div>\
+          </a>\
+          <div id="mediafront_totaltime" class="mediaplayer_controlspace">00:00</div>\
+          <div id="mediafront_audio" class="mediaplayer_controlspace">\
+             <div id="mediaplayer_audioButton"></div>\
+             <div id="mediaplayer_audioBar"></div>\
+          </div>\
+          <a id="mediafront_information" class="mediaplayerlink mediaplayer_controlspace" href="#menu">\
+            <div id="mediafront_menuButton"><span>information</span></div>\
+          </a>\
+        </div>\
+        <div id="mediaplayer_seekBar">\
+          <div id="mediaplayer_seekProgress"></div>\
+        </div>\
+      </div>');
+    }
+
+    // Let our template know we have a controller.
+    this.context.addClass('with-controller');
+    return jQuery('#mediaplayer_control', this.context);
+  }
+
+  // Return the elements
+  osmplayer.controller[template].prototype.getElements = function() {
+    var elements = minplayer.controller.prototype.getElements.call(this);
+    return jQuery.extend(elements, {
+      play: jQuery('#mediaplayer_play', this.display),
+      pause: jQuery('#mediaplayer_pause', this.display),
+      fullscreen: jQuery('#mediafront_resizeScreen', this.display),
+      seek: jQuery('#mediaplayer_seekBar', this.display),
+      progress: jQuery('#mediaplayer_seekProgress', this.display),
+      volume: jQuery('#mediaplayer_audioBar', this.display),
+      mute: jQuery('#mediaplayer_audioButton', this.display),
+      timer: jQuery('#mediafront_playtime', this.display),
+      duration: jQuery('#mediafront_totaltime', this.display)
+    });
+  };
+})('simpleblack', osmplayer);
---
+++ templates/simpleblack/js/osmplayer.simpleblack.js
@@ -0,0 +1,88 @@
+(function(template, osmplayer) {
+
+  /** The osmplayer namespace. */
+  var osmplayer = osmplayer || {};
+
+  // templated player.
+  osmplayer[template] = function(context, options) {
+
+    // Make sure we provide default options...
+    this.options = jQuery.extend({
+      controllerOnly: false
+    }, this.options);
+
+    // Derive from osmplayer.
+    osmplayer.call(this, context, options);
+  };
+
+  /**
+   * Define this template prototype.
+   */
+  osmplayer[template].prototype = new osmplayer();
+  osmplayer[template].prototype.constructor = osmplayer[template];
+
+  /**
+   * The player constructor.
+   */
+  osmplayer[template].prototype.construct = function() {
+    osmplayer.prototype.construct.call(this);
+    if (this.options.controllerOnly) {
+      this.display.addClass('controller-only');
+    }
+  };
+
+  /**
+   * Return the display for this plugin.
+   */
+  osmplayer[template].prototype.getDisplay = function() {
+
+    // If this is the bottom element, then we need to build.
+    if (this.context.children().length == 0) {
+
+      // Build out the player provided the base tag.
+      this.context = this.context.attr({
+        'id': this.options.id + '-player',
+        'class': 'mediaplayer-' + template + '-media'
+      })
+      .wrap(jQuery(document.createElement('div')).attr({
+        'id': 'mediaplayer_display'
+      })).parent('#mediaplayer_display')
+      .wrap(jQuery(document.createElement('div')).attr({
+        'id': 'mediaplayer_minplayer'
+      })).parent('#mediaplayer_minplayer')
+      .prepend('\
+        <div id="mediaplayer_busy"></div>\
+        <div id="mediaplayer_bigPlay"></div>\
+        <div id="mediaplayer_preview"></div>\
+      ')
+      .wrap(jQuery(document.createElement('div')).attr({
+        'id': 'mediaplayer_node'
+      })).parent('#mediaplayer_node')
+      .wrap(jQuery(document.createElement('div')).attr({
+        'id': 'mediaplayer'
+      })).parent('#mediaplayer')
+      .wrap(jQuery(document.createElement('div')).attr({
+        'id': this.options.id,
+        'class': 'osmplayer-' + template + ' player-ui'
+      })).parent('.osmplayer-' + template);
+
+      // Mark a flag that says this display needs to be built.
+      this.options.build = true;
+    }
+
+    return this.context;
+  }
+
+  // Get the elements for this player.
+  osmplayer[template].prototype.getElements = function() {
+    var elements = osmplayer.prototype.getElements.call(this);
+    this.display.width(this.options.width);
+    this.display.height(this.options.height);
+    return jQuery.extend(elements, {
+      player:this.display,
+      minplayer: jQuery('#mediaplayer_minplayer', this.display),
+      display:jQuery('#mediaplayer_display', this.display),
+      media:jQuery('.minplayer-' + template + '-media', this.display)
+    });
+  };
+})('simpleblack', osmplayer);
