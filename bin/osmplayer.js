3a8e16cab2a2b44ca9634df2d36ade995ac5bfbd
Travis Tidwell <travist349@gmail.com>
2012-05-15 20:35:43 -0700

    Fixed some issues with the YouTube player and PlayLoader if more media is loaded.

--- bin/osmplayer.js
+++ bin/osmplayer.js
@@ -3000,6 +3000,9 @@ minplayer.playLoader = function(context, options) {
   /** The preview image. */
   this.preview = null;
 
+  /** If the playLoader is enabled. */
+  this.enabled = true;
+
   // Derive from display
   minplayer.display.call(this, 'playLoader', context, options);
 };
@@ -3023,6 +3026,9 @@ minplayer.playLoader.prototype.construct = function() {
 
     // Only bind if this player does not have its own play loader.
     if (!media.hasPlayLoader()) {
+
+      // Enable the playLoader.
+      this.enabled = true;
 
       // Get the poster image.
       if (!this.options.preview) {
@@ -3087,6 +3093,7 @@ minplayer.playLoader.prototype.construct = function() {
     else {
 
       // Hide the display.
+      this.enabled = false;
       this.hide(this.elements.busy);
       this.hide(this.elements.bigPlay);
       this.hide();
@@ -3101,6 +3108,11 @@ minplayer.playLoader.prototype.construct = function() {
  * Loads the preview image.
  */
 minplayer.playLoader.prototype.loadPreview = function() {
+
+  // Ignore if disabled.
+  if (!this.enabled) {
+    return;
+  }
 
   // If the preview element exists.
   if (this.elements.preview) {
@@ -3130,6 +3142,11 @@ minplayer.playLoader.prototype.loadPreview = function() {
  * button.
  */
 minplayer.playLoader.prototype.checkVisibility = function() {
+
+  // Ignore if disabled.
+  if (!this.enabled) {
+    return;
+  }
 
   // Hide or show the busy cursor based on the flags.
   if (this.busy.flag) {
@@ -4775,56 +4792,54 @@ minplayer.players.youtube.prototype.create = function() {
   this.playerId = this.options.id + '-player';
 
   // Poll until the YouTube API is ready.
-  window.onYouTubePlayerAPIReady = (function(player) {
+  this.poll((function(player) {
     return function() {
-      player.poll(function() {
-        var ready = jQuery('#' + player.playerId).length > 0;
-        if (ready) {
-          // Determine the origin of this script.
-          var origin = location.protocol;
-          origin += '//' + location.hostname;
-          origin += (location.port && ':' + location.port);
+      var ready = jQuery('#' + player.playerId).length > 0;
+      if (ready && YT && typeof YT.Player == 'function') {
+        // Determine the origin of this script.
+        var origin = location.protocol;
+        origin += '//' + location.hostname;
+        origin += (location.port && ':' + location.port);
 
-          var playerVars = {};
-          if (minplayer.isIDevice) {
-            playerVars.origin = origin;
-          }
-          else {
-            playerVars = {
-              enablejsapi: minplayer.isIDevice ? 0 : 1,
-              origin: origin,
-              wmode: 'opaque',
-              controls: minplayer.isAndroid ? 1 : 0
-            };
-          }
-
-          // Create the player.
-          player.player = new YT.Player(player.playerId, {
-            height: '100%',
-            width: '100%',
-            frameborder: 0,
-            videoId: player.mediaFile.id,
-            playerVars: playerVars,
-            events: {
-              'onReady': function(event) {
-                player.onReady(event);
-              },
-              'onStateChange': function(event) {
-                player.onPlayerStateChange(event);
-              },
-              'onPlaybackQualityChange': function(newQuality) {
-                player.onQualityChange(newQuality);
-              },
-              'onError': function(errorCode) {
-                player.onError(errorCode);
-              }
-            }
-          });
+        var playerVars = {};
+        if (minplayer.isIDevice) {
+          playerVars.origin = origin;
         }
-        return !ready;
-      }, 200);
+        else {
+          playerVars = {
+            enablejsapi: minplayer.isIDevice ? 0 : 1,
+            origin: origin,
+            wmode: 'opaque',
+            controls: minplayer.isAndroid ? 1 : 0
+          };
+        }
+
+        // Create the player.
+        player.player = new YT.Player(player.playerId, {
+          height: '100%',
+          width: '100%',
+          frameborder: 0,
+          videoId: player.mediaFile.id,
+          playerVars: playerVars,
+          events: {
+            'onReady': function(event) {
+              player.onReady(event);
+            },
+            'onStateChange': function(event) {
+              player.onPlayerStateChange(event);
+            },
+            'onPlaybackQualityChange': function(newQuality) {
+              player.onQualityChange(newQuality);
+            },
+            'onError': function(errorCode) {
+              player.onError(errorCode);
+            }
+          }
+        });
+      }
+      return !ready;
     };
-  })(this);
+  })(this), 200);
 
   // Return the player.
   return jQuery(document.createElement('div')).attr({
