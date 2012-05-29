c9ac25aee9ca671a8daec70e6b858008ab905b5f
Travis Tidwell <travist349@gmail.com>
2012-05-29 11:59:54 -0700

    Fixed an issue where it was adding swfobject more than once.

--- minplayer/src/minplayer.players.flash.js
+++ minplayer/src/minplayer.players.flash.js
@@ -64,7 +64,7 @@ minplayer.players.flash.prototype.getFlash = function(params) {
 
   // Create the swfobject.
   setTimeout((function(player) {
-    return function() {
+    return function tryAgain() {
       if (swfobject) {
         swfobject.embedSWF(
           params.swf,
@@ -91,7 +91,9 @@ minplayer.players.flash.prototype.getFlash = function(params) {
         );
       }
       else {
-        player.getFlash(params);
+
+        // Try again after 200 ms.
+        setTimeout(tryAgain, 200);
       }
     };
   })(this), 200);
