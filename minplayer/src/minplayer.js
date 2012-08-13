abf8e84468823ce409fe0649b8a29f543ca37c37
Travis Tidwell <travist349@gmail.com>
2012-08-12 19:36:41 -0700

    Fixed an issue where vimeo and youtube were not playing nice together.

--- minplayer/src/minplayer.js
+++ minplayer/src/minplayer.js
@@ -190,24 +190,26 @@ minplayer.prototype.addEvents = function() {
  * @param {string} error The error to display on the player.
  */
 minplayer.prototype.showError = function(error) {
-  error = error || '';
-  if (this.elements.error) {
+  if (typeof error !== 'object') {
+    error = error || '';
+    if (this.elements.error) {
 
-    // Set the error text.
-    this.elements.error.text(error);
-    if (error) {
-      // Show the error message.
-      this.elements.error.show();
+      // Set the error text.
+      this.elements.error.text(error);
+      if (error) {
+        // Show the error message.
+        this.elements.error.show();
 
-      // Only show this error for a time interval.
-      setTimeout((function(player) {
-        return function() {
-          player.elements.error.hide('slow');
-        };
-      })(this), 5000);
-    }
-    else {
-      this.elements.error.hide();
+        // Only show this error for a time interval.
+        setTimeout((function(player) {
+          return function() {
+            player.elements.error.hide('slow');
+          };
+        })(this), 5000);
+      }
+      else {
+        this.elements.error.hide();
+      }
     }
   }
 };
