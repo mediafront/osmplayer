fdac2fd93fa4485304d1fbb7fdd61e8a9d8a1c1c
Travis Tidwell <travist349@gmail.com>
2018-12-26 21:00:23 +0300

    Removed the links from the player.

--- templates/default/osmplayer_default_playlist.tpl.php
+++ templates/default/osmplayer_default_playlist.tpl.php
@@ -1,7 +1,6 @@
 <div id="<?php print $params['prefix']; ?>mediaplaylist">
   <div id="<?php print $params['prefix']; ?>mediascrollwrapper">
     <div id="<?php print $params['prefix']; ?>mediabusy" class="<?php print $params['prefix']; ?>ui-state-default <?php print $params['prefix']; ?>ui-state-disabled"><img src="<?php print $params['playerPath']; ?>templates/default/images/busy.gif" /></div>
-    <?php print $templates['links']; ?>
     <div id="<?php print $params['prefix']; ?>mediascroll" class="<?php print $params['prefix']; ?>ui-helper-clearfix">
       <?php if( $params['showScrollbar'] ) { print $templates['scrollBar']; } ?>
       <div id="<?php print $params['prefix']; ?>medialistmask">
