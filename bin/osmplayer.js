421a1d4676a6e010ab6e2427232a188a0cc497d5
Travis Tidwell <travist349@gmail.com>
2019-03-15 07:13:55 +0300

    Fixed the comment on the overridden method.

--- bin/osmplayer.js
+++ bin/osmplayer.js
@@ -1962,7 +1962,7 @@ minplayer.display.prototype.getDisplay = function() {
 };
 
 /**
- * Returns if this component is valid.
+ * @see minplayer.plugin.isValid
  *
  * @return {boolean} TRUE if the plugin display is valid.
  */
