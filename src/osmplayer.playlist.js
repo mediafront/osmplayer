a7d4640f128f773d4e2856d29c999e741a9cad3f
Travis Tidwell <travist349@gmail.com>
2019-03-28 05:59:47 +0300

    Fixed the playlist from making the player loop when no playlist is provided...

--- src/osmplayer.playlist.js
+++ src/osmplayer.playlist.js
@@ -151,20 +151,21 @@ osmplayer.playlist.prototype.construct = function() {
     };
   })(this));
 
-  // Get the media.
-  if (this.options.autoNext) {
-    this.get('media', function(media) {
-      media.bind('ended', (function(playlist) {
-        return function(event) {
-          media.options.autoplay = true;
-          playlist.next();
-        };
-      })(this));
-    });
-  }
-
   // Load the "next" item.
-  this.next();
+  if (this.next()) {
+
+    // Get the media.
+    if (this.options.autoNext) {
+      this.get('media', function(media) {
+        media.bind('ended', (function(playlist) {
+          return function(event) {
+            media.options.autoplay = true;
+            playlist.next();
+          };
+        })(this));
+      });
+    }
+  }
 
   // Say that we are ready.
   this.ready();
@@ -301,6 +302,8 @@ osmplayer.playlist.prototype.setQueue = function() {
 
 /**
  * Loads the next item.
+ *
+ * @return {boolean} TRUE if loaded, FALSE if not.
  */
 osmplayer.playlist.prototype.next = function() {
   var item = 0, page = this.page;
@@ -313,17 +316,17 @@ osmplayer.playlist.prototype.next = function() {
       item = Math.floor(Math.random() * this.totalItems);
       page = Math.floor(item / this.options.pageLimit);
       item = item % this.options.pageLimit;
-      this.load(page, item);
+      return this.load(page, item);
     }
     else {
 
       // Otherwise, increment the current item by one.
       item = (this.currentItem + 1);
       if (item >= this.nodes.length) {
-        this.load(page + 1, 0);
+        return this.load(page + 1, 0);
       }
       else {
-        this.loadItem(item);
+        return this.loadItem(item);
       }
     }
   }
@@ -332,12 +335,14 @@ osmplayer.playlist.prototype.next = function() {
     // Load the next item in the playqueue.
     this.playqueuepos = this.playqueuepos + 1;
     var currentQueue = this.playqueue[this.playqueuepos];
-    this.load(currentQueue.page, currentQueue.item);
+    return this.load(currentQueue.page, currentQueue.item);
   }
 };
 
 /**
  * Loads the previous item.
+ *
+ * @return {boolean} TRUE if loaded, FALSE if not.
  */
 osmplayer.playlist.prototype.prev = function() {
 
@@ -346,14 +351,16 @@ osmplayer.playlist.prototype.prev = function() {
   this.playqueuepos = (this.playqueuepos < 0) ? 0 : this.playqueuepos;
   var currentQueue = this.playqueue[this.playqueuepos];
   if (currentQueue) {
-    this.load(currentQueue.page, currentQueue.item);
+    return this.load(currentQueue.page, currentQueue.item);
   }
+  return false;
 };
 
 /**
  * Loads a playlist node.
  *
  * @param {number} index The index of the item you would like to load.
+ * @return {boolean} TRUE if loaded, FALSE if not.
  */
 osmplayer.playlist.prototype.loadItem = function(index) {
   if (index < this.nodes.length) {
@@ -368,25 +375,30 @@ osmplayer.playlist.prototype.loadItem = function(index) {
     teaser = this.nodes[index];
     teaser.select(true);
     this.trigger('nodeLoad', teaser.node);
+    return true;
   }
+
+  return false;
 };
 
 /**
  * Loads the next page.
  *
  * @param {integer} loadIndex The index of the item to load.
+ * @return {boolean} TRUE if loaded, FALSE if not.
  */
 osmplayer.playlist.prototype.nextPage = function(loadIndex) {
-  this.load(this.page + 1, loadIndex);
+  return this.load(this.page + 1, loadIndex);
 };
 
 /**
  * Loads the previous page.
  *
  * @param {integer} loadIndex The index of the item to load.
+ * @return {boolean} TRUE if loaded, FALSE if not.
  */
 osmplayer.playlist.prototype.prevPage = function(loadIndex) {
-  this.load(this.page - 1, loadIndex);
+  return this.load(this.page - 1, loadIndex);
 };
 
 /**
@@ -394,12 +406,13 @@ osmplayer.playlist.prototype.prevPage = function(loadIndex) {
  *
  * @param {integer} page The page to load.
  * @param {integer} loadIndex The index of the item to load.
+ * @return {boolean} TRUE if loaded, FALSE if not.
  */
 osmplayer.playlist.prototype.load = function(page, loadIndex) {
 
   // If the playlist and pages are the same, then no need to load.
   if ((this.playlist == this.options.playlist) && (page == this.page)) {
-    this.loadItem(loadIndex);
+    return this.loadItem(loadIndex);
   }
 
   // Set the new playlist.
@@ -407,7 +420,7 @@ osmplayer.playlist.prototype.load = function(page, loadIndex) {
 
   // Return if there aren't any playlists to play.
   if (!this.playlist) {
-    return;
+    return false;
   }
 
   // Determine if we need to loop.
@@ -418,7 +431,7 @@ osmplayer.playlist.prototype.load = function(page, loadIndex) {
       loadIndex = 0;
     }
     else {
-      return;
+      return false;
     }
   }
 
@@ -451,7 +464,7 @@ osmplayer.playlist.prototype.load = function(page, loadIndex) {
     if (this.playlist.endpoint) {
       this.playlist = this.options.playlist = this.playlist.endpoint;
     }
-    return;
+    return true;
   }
 
   // Get the highest priority parser.
@@ -503,4 +516,7 @@ osmplayer.playlist.prototype.load = function(page, loadIndex) {
 
   // Perform an ajax callback.
   jQuery.ajax(request);
+
+  // Return that we did something.
+  return true;
 };
