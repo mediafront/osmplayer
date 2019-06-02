f1be9ed8ad07af2c7d293afd9751a1381d9598ad
Travis Tidwell <travist349@gmail.com>
2019-06-03 01:47:31 +0300

    Moved the player class to the main player.

--- minplayer/bin/minplayer.js
+++ minplayer/bin/minplayer.js
@@ -1626,6 +1626,7 @@ minplayer.prototype.loadPlayer = function() {
 
         // Load the media.
         media.load(player.options.file);
+        player.display.addClass('minplayer-player-' + media.mediaFile.player);
       };
     })(this));
 
@@ -1637,7 +1638,9 @@ minplayer.prototype.loadPlayer = function() {
 
     // Now load the different media file.
     this.media.options = this.options;
+    this.display.removeClass('minplayer-player-' + this.media.mediaFile.player);
     this.media.load(this.options.file);
+    this.display.addClass('minplayer-player-' + this.media.mediaFile.player);
     return false;
   }
 };
