4d442a3b7c335bc602c82404e341f2d21cd8f83a
Travis Tidwell <travist349@gmail.com>
2019-11-11 05:52:08 +0300

    Fixed the preview so that it will laod without media.

--- bin/osmplayer.js
+++ bin/osmplayer.js
@@ -6333,13 +6333,19 @@ osmplayer.prototype.loadNode = function(node) {
         };
       })(this));
     }
+    else {
+
+      // Add a class to the display to let themes handle this.
+      this.display.addClass('nomedia');
+    }
 
     // Load the preview image.
     osmplayer.getImage(node.mediafiles, 'preview', (function(player) {
       return function(image) {
         player.options.preview = image.path;
         if (player.playLoader) {
-          player.playLoader.initialize();
+          player.playLoader.enabled = true;
+          player.playLoader.loadPreview();
         }
       };
     })(this));
